4d8747dc890f4f8b8412de01784f99896898ef7e
Stephan Djurhuus <stephan.mdd@gmail.com>
2022-01-14 13:30:30 +0100

    connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -43,7 +43,7 @@ const dbConfig = {
 	server: 'localhost', //process.env.SERVER_IP,
 	database: 'master', //process.env.DATABASE,
 	user: 'sa', //process.env.DATABASE_USERNAME,
-	password: '', //process.env.PASSWORD,
+	password: 'pass1234', //process.env.PASSWORD,
 	port: 1433, //parseInt(process.env.PORT),
 	options: {
 		encrypt: false,
